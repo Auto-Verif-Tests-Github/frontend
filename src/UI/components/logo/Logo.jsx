c5d68a96a9b7205a5b02395b1e604cafc521c950
Eugene <genyamateyuk@gmail.com>
2023-02-24 20:53:09 +0700

    added redirect on / for click event

--- src/UI/components/logo/Logo.jsx
+++ src/UI/components/logo/Logo.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import cs from "./Logo.module.css";
+import {useNavigate} from "react-router-dom";
 
 const Logo = ({className}) => {
+    const navigate = useNavigate();
     return (
-        <div><span className={cs.logo + " " + className}>Sys.Pro</span></div>
+        <div onClick={() => navigate("/")}><span className={cs.logo + " " + className}>Sys.Pro</span></div>
     )
 }
 
