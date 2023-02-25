137de21835f598ed29610725108d03d0a8c49e07
Eugene <genyamateyuk@gmail.com>
2023-02-26 03:26:11 +0700

    transition to login is removed if the user is authorized

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Logo from "../UI/components/logo/Logo";
 import cs from "./HomePage.module.css";
 import {Link, useNavigate} from "react-router-dom";
 import TwitterButton from "../UI/components/buttons/TwitterButton";
 import education from "../images/education_icon.png";
+import {isAuthorized} from "../api/auth/auth"
 
 const HomePage = () => {
+    const [auth, setAuth] = useState(false);
+    useEffect(() => setAuth(isAuthorized), []);
     const navigate = useNavigate();
 
     return (
         <div className={cs.home_page}>
             <Logo className={cs.logo} />
             <TwitterButton onClick={() => navigate("/streams")} buttonStyle={cs.btn} imgSrc={education} value="Перейти к выбору потока" />
-            <Link className={cs.teacher} to="/login">Я преподаватель!</Link>
+            {!auth
+                ? <Link className={cs.teacher} to="/login">Я преподаватель!</Link>
+                : ""}
         </div>
     );
 }
