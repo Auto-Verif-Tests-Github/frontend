d3cfbffe261e0dffa8f66da3ff1ae55532e80891
Eugene <genyamateyuk@gmail.com>
2023-02-25 20:01:35 +0700

    mouse event

--- src/UI/components/table/CourseTable.jsx
+++ src/UI/components/table/CourseTable.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import cs from "./CourseTable.module.css";
+import Popup from "../popup/Popup";
 
 /*
 * tableInfo - LineTable
@@ -7,9 +8,20 @@ import cs from "./CourseTable.module.css";
 */
 const CourseTable = ({courseName, tableInfo, tasks}) => {
     const [info, setInfo] = useState(tableInfo);
+    const [popup, setPopup] = useState("");
+
+    function showPopup(e, text) {
+        setPopup(() => <Popup
+            onDelete={() => setPopup("")}
+            onDeleteTimeout={2000}
+            value={text}
+            fixX={e.pageX} fixY={e.pageY}
+        />);
+    }
 
     return (
         <div className={cs.course}>
+            {popup}
             <span className={cs.table_name}>{courseName}</span>
             <table className={cs.table} border="1">
                 <tbody>
@@ -19,11 +31,11 @@ const CourseTable = ({courseName, tableInfo, tasks}) => {
                     </tr>
                     <tr>
                         <th>Студент</th>
-                        {tasks.map(e => <th key={e.id}>{e.date}</th>)}
+                        {tasks.map(e => <th onMouseEnter={(e) => showPopup(e, "Дата создания")} key={e.id}>{e.date}</th>)}
                     </tr>
                     <tr className={cs.deadline}>
                         <th>-</th>
-                        {tasks.map(e => <th key={e.id}>{e.deadline_date}</th>)}
+                        {tasks.map(e => <th onMouseEnter={(e) => showPopup(e, "Дата дедлайна")} key={e.id}>{e.deadline_date}</th>)}
                     </tr>
                 </tbody>
                 {info}
