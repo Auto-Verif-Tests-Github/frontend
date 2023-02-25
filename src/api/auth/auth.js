62fc002bca2d1d98457909a9dc7c610fce6d1046
Eugene <genyamateyuk@gmail.com>
2023-02-26 04:35:46 +0700

    added password hash

--- src/api/auth/auth.js
+++ src/api/auth/auth.js
@@ -1,19 +1,22 @@
 const name = "is_auth";
 const auth = "true";
 const deauth = "false";
+const hash = "pass_hash";
 
 export function isAuthorized() {
     const status = localStorage.getItem(name);
     if(status == null) {
-        localStorage.setItem(name, deauth);
+        deauthorize();
     }
     return status === auth;
 }
 
-export function authorize() {
+export function authorize(passHash) {
     localStorage.setItem(name, auth);
+    localStorage.setItem(hash, passHash);
 }
 
 export function deauthorize() {
     localStorage.setItem(name, deauth);
+    localStorage.setItem(hash, "");
 }
