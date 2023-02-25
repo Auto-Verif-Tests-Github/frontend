e36dfcda7649b9ff99a688f940078c6963faf584
Eugene <genyamateyuk@gmail.com>
2023-02-26 03:24:59 +0700

    edit/fixed table

--- src/UI/components/table/CourseTable.jsx
+++ src/UI/components/table/CourseTable.jsx
@@ -24,21 +24,20 @@ const CourseTable = ({courseName, tableInfo, tasks}) => {
             {popup}
             <span className={cs.table_name}>{courseName}</span>
             <table className={cs.table} border="1">
-                <tbody>
+                <thead>
                     <tr>
                         <th>Название</th>
                         {tasks.map(e => <th key={e.id}>{e.name}</th>)}
                     </tr>
                     <tr>
-                        <th>Студент</th>
+                        <th rowSpan={2}>Студенты</th>
                         {tasks.map(e => <th onMouseEnter={(e) => showPopup(e, "Дата создания")} key={e.id}>{e.date}</th>)}
                     </tr>
                     <tr className={cs.deadline}>
-                        <th>-</th>
                         {tasks.map(e => <th onMouseEnter={(e) => showPopup(e, "Дата дедлайна")} key={e.id}>{e.deadline_date}</th>)}
                     </tr>
-                </tbody>
-                {info}
+                </thead>
+                <tbody>{info}</tbody>
             </table>
         </div>
     );
--- src/pages/CourseViewPage.jsx
+++ src/pages/CourseViewPage.jsx
@@ -11,12 +11,12 @@ const CourseViewPage = () => {
             <CourseTable
                 courseName="Наукоемкое программирование на Python"
                 tableInfo={[
-                    <LineTable personName="Петр Иванов" solutions={[{task_id: 1, status: "Р", task_position_id: 0}]} />,
-                    <LineTable personName="Вася Андреев" solutions={[{task_id: 1, status: "Р", task_position_id: 0}]} />,
-                    <LineTable personName="Николай Сидоров" solutions={[{task_id: 1, status: "Р", task_position_id: 0}]} />,
-                    <LineTable personName="Жанна Горевая" solutions={[{task_id: 1, status: "Р", task_position_id: 0}]} />,
-                    <LineTable personName="Артур Третий" solutions={[{task_id: 1, status: "П", task_position_id: 0}, {task_id: 50, status: "5б", task_position_id: 1}]} />,
-                    <LineTable personName="Жак Польских" solutions={[{task_id: 1, status: "Р", task_position_id: 0}]} />
+                    <LineTable key={1} personName="Петр Иванов" solutions={[{task_id: 1, status: "Р", task_position_id: 0}]} />,
+                    <LineTable key={2} personName="Вася Андреев" solutions={[{task_id: 1, status: "Р", task_position_id: 0}]} />,
+                    <LineTable key={3} personName="Николай Сидоров" solutions={[{task_id: 1, status: "Р", task_position_id: 0}]} />,
+                    <LineTable key={4} personName="Жанна Горевая" solutions={[{task_id: 1, status: "Р", task_position_id: 0}]} />,
+                    <LineTable key={5} personName="Артур Третий" solutions={[{task_id: 1, status: "П", task_position_id: 0}, {task_id: 50, status: "5б", task_position_id: 1}]} />,
+                    <LineTable key={6} personName="Жак Польских" solutions={[{task_id: 1, status: "Р", task_position_id: 0}]} />
                 ]}
                 tasks={[
                     {id: 1, id_position: 0, date: "02.02.2001", deadline_date: "03.02.2001", name: "Мини-Задача 1"},
